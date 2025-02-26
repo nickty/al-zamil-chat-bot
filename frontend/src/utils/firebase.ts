06b27d4ec29e250274ea1f2eaf4f474777533b4a
znickty <mizan.rahman@zhiltd.com>
2025-02-26 16:05:39 +0300

    firebase info protected

--- frontend/src/utils/firebase.ts
+++ frontend/src/utils/firebase.ts
@@ -20,13 +20,12 @@ import { getStorage } from "firebase/storage"
 // }
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBQTrNHxkEV0JGJ87xiWuZq3-1C5m9FVlc",
-    authDomain: "zhi-assistant-453d0.firebaseapp.com",
-    projectId: "zhi-assistant-453d0",
-    storageBucket: "zhi-assistant-453d0.firebasestorage.app",
-    messagingSenderId: "1059535786942",
-    appId: "1:1059535786942:web:c64fd7689920928ca95fef",
-    measurementId: "G-7LT27C9VHH"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
   };
 
 // export const app = initializeApp(firebaseConfig)
